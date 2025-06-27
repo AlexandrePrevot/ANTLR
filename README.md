6b1d3d64ffc9d7c67617ced0d60e7b504b256ebf
Alexandre Prevot <alexandre.prevot91@gmail.com>
2025-06-27 11:47:06 +0200

    progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 how to build :
 follow https://github.com/antlr/antlr4/tree/master/runtime/Cpp/cmake
 based on https://tomassetti.me/getting-started-antlr-cpp/
+
+java -jar /usr/local/lib/java/antlr-4.13.0-complete.jar -Dlanguage=Cpp -listener -visitor -o ../generated/ -package antlrcpptest ../grammar/Scene.g4
--- src/parser/Parser.cc
+++ src/parser/Parser.cc
@@ -1,13 +1,29 @@
-#include <iostream>
-
 #include "parser/Parser.h"
 
+#include <iostream>
 #include "antlr4-runtime.h"
-#include "TLexer.h"
-#include "TParser.h"
+#include "SceneLexer.h"
+#include "SceneParser.h"
+//#include "ImageVisitor.h"
+
+using namespace std;
+using namespace antlr4;
+
 
 bool Parser::parse() {
-    std::cout << "I am parsing" << std::endl;
+    //std::cout << "I am parsing" << std::endl;
+
+    std::ifstream stream;
+    stream.open("input.scene");
+
+    ANTLRInputStream input(stream);
+    antlrcpptest::SceneLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+    antlrcpptest::SceneParser parser(&tokens);    
+    antlrcpptest::SceneParser::FileContext* tree = parser.file();
+    //ImageVisitor visitor;
+    //Scene scene = std::any_cast<Scene>(visitor.visitFile(tree));
+    //scene.draw();
 
     return true;
 }
